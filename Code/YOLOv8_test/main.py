a30b100056f46fd75fa3e6071a2a4840558d0209
kadirhan0168 <113602149+kadirhan0168@users.noreply.github.com>
2024-02-28 12:30:20 +0100

    Update main.py

--- Code/YOLOv8_test/main.py
+++ Code/YOLOv8_test/main.py
@@ -1,5 +1,4 @@
 from ultralytics import YOLO            # pip install ultralytics
-import cv2
 
 model = YOLO("yolov8x.pt")              # model van yolov8 zelf
 results = model.predict(source="0", show=True)
