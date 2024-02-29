002f88be5a2608be315a6a0747e3eee3513f99d7
1056357 <125043096+1056357@users.noreply.github.com>
2024-02-29 10:17:26 +0100

    Create lijncode.py

---
+++ Code/lijncode.py
@@ -0,0 +1,57 @@
+import numpy as np
+import cv2 as cv
+
+def detect_lines(frame):
+    # Blurr the image and then turn into greyscale
+    kernel_size = 7
+    blur = cv.GaussianBlur(frame, (kernel_size, kernel_size), 0)
+    grey = cv.cvtColor(blur, cv.COLOR_BGR2GRAY)
+
+    # Lower and upper bound for detecting white
+    lower_white = 240
+    upper_white = 255
+    white = cv.inRange(grey, lower_white, upper_white)  # filters only white
+
+    # Process edge detection, use Canny
+    low_threshold = 50
+    high_threshold = 400
+    edges = cv.Canny(white, low_threshold, high_threshold)
+    return edges
+
+def main():
+    # Open the default camera
+    cap = cv.VideoCapture(0)
+
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        
+        # Detect lines
+        edges = detect_lines(frame)
+        
+        # Display the resulting frame
+        cv.imshow('Live Edge Detection', edges)
+
+        # Check if line is detected
+        if np.sum(edges) > 500:  # This means there are white pixels in the edges image
+            print("Line detected")
+        
+        # Break the loop
+        if cv.waitKey(1) == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
