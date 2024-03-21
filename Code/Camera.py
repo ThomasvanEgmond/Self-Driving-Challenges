dddb844bb930cd38ea07da058b91322bfaa59498
Luukdvr2 <120176492+luukdvr@users.noreply.github.com>
2024-03-21 10:54:50 +0100

    ewtrw

--- Code/Camera.py
+++ Code/Camera.py
@@ -1,10 +1,9 @@
 import cv2 as cv
 class Camera:
-    def __init__(self, name, cap, base_value, type, keyboardInputs, lower_white=100) -> None:
+    def __init__(self, name, cap, base_value, keyboardInputs, lower_white=100) -> None:
         self.name=name
         self.cap=cv.VideoCapture(cap, cv.CAP_DSHOW)
         self.base_value=base_value 
-        self.type=type
         self.keyboardInputs=keyboardInputs # string of 5 chars for controls
         self.lower_white=lower_white
         self.calibrationCountDown=0
