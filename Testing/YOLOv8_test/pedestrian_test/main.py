272f62ec6df797908dd945b826a6e2be514dee9e
1056357 <125043096+1056357@users.noreply.github.com>
2024-04-16 11:35:35 +0200

    person crossing with segment

--- Testing/YOLOv8_test/pedestrian_test/main.py
+++ Testing/YOLOv8_test/pedestrian_test/main.py
@@ -12,15 +12,15 @@ ov_model = YOLO('yolov8n_openvino_model/', task='detect')
 camera = cv2.VideoCapture(0)
 framecount = 0
 
-personCrossing = False
-startSegment = 0 # segment where person is first detected
-currentSegment = 0 # current segment of person during crossing
+personCrossing = False # is true while a person is crossing
+personCrossingDone = False # is true after a person has crossed and resets when no person detected
+startSegment = -1 # segment where person is first detected
+currentSegment = -1 # current segment of person during crossing
+
+segmentCount = 4
 
 def normalToSegment(n):
-    return int(n*4)
-
-# def xyxyToCenter(x0,y0,x1,y1):
-#     return x0+(x1-x0)/2, y0+(y1-y0)/2
+    return int(n*segmentCount)
 
 def personDetection(frame, saveFrame=False):
     results = ov_model.predict(frame, classes=[0], conf=0.5, verbose=False) # find persons in view
@@ -29,18 +29,55 @@ def personDetection(frame, saveFrame=False):
         if saveFrame: result.save("images/pedestrian_test_frame.png")
         # print(result.boxes.cls)
         boxes = result.boxes.xyxyn.tolist()
-        if len(boxes) == 0: personCrossing = False
-        if len(boxes) != 0: personCrossing = True
         print(f"{len(boxes)} person detected")
         # print(boxes)
 
-        # box = boxes[0]
+        global personCrossing
+        global personCrossingDone
+        global startSegment
 
-        for box in boxes:
-            segmentCenter = normalToSegment(box[0]+(box[2]-box[0])/2)
-            for i in box:
-                print(round(i,2), end=",\t") # print normalised coördinates
-            print(f"segment min-max: {normalToSegment(box[0])}-{normalToSegment(box[2])}, segment center: {segmentCenter} ")
+        # get person box
+        personBox = []
+        if len(boxes) == 0: 
+            if personCrossingDone: 
+                print("crossing reset")
+                personCrossingDone = False
+            break
+        if len(boxes) == 1: personBox = boxes[0] 
+        if len(boxes) > 1: # find largest person box 
+            largestSize = 0
+            largestBoxIndex = 0
+            for i,box in enumerate(boxes):
+                boxSize = (box[2]-box[0])*(box[3]-box[1])
+                print(f"\tbox {i}: size = {boxSize}")
+                if boxSize > largestSize: 
+                    largestSize = boxSize
+                    largestBoxIndex = i
+            print(f"largest box is box {largestBoxIndex}")
+            personBox = boxes[largestBoxIndex]
+
+        # find segment 
+        segmentCenter = normalToSegment(personBox[0]+(personBox[2]-personBox[0])/2)
+        boxSize = (personBox[2]-personBox[0])*(personBox[3]-personBox[1])
+        for i in personBox:
+            print(f"\t{round(i,2)},", end="") # print normalised coördinates
+        print(f"\tsize: {round(boxSize,2)},\tsegment min-max: {normalToSegment(personBox[0])}-{normalToSegment(personBox[2])}, segment center: {segmentCenter} ")
+        currentSegment = segmentCenter
+        # print(f"\tcurrentSegment = {currentSegment}")
+
+        # start crossing
+        if not personCrossing and not personCrossingDone:
+            startSegment = currentSegment
+            print(f"stop driving, startSegment = {startSegment}")
+            personCrossing = True
+
+        # stop crossing
+        elif personCrossing and currentSegment != startSegment and (currentSegment == 0 or currentSegment == segmentCount-1) :
+            print("start driving")
+            personCrossing = False
+            personCrossingDone = True
+        
+
 
 # personDetection(cv2.imread("images/Afbeelding5.png"), True)
 
@@ -51,6 +88,7 @@ while True:
     if not ret:
         print("failed to grab frame")
         break
+    
     cv2.imshow("test", frame)
 
     if framecount%50==0:
