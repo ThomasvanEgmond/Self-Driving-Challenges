4a3e45a68566da79f7bf5eb18feb44ba4834b2e9
ThomasvanEgmond <122525080+ThomasvanEgmond@users.noreply.github.com>
2024-04-23 15:53:37 +0200

    Added IPC and communication to the ESP32

--- Pre-Production/Intel NUC/ESP.py
+++ Pre-Production/Intel NUC/ESP.py
@@ -1,11 +1,44 @@
 import serial
 import serial.tools.list_ports
+import threading
+import time
 
 class ESP32:
     def __init__(self):
         self.ser = None
+        self.motorPWM = 0
+        self.brakePWM = 0
+        self.steeringDegrees = 90
+        self.state = "driving"
+        self.childPipe = None
 
-    def connect(self, ):
+    def getParentData(self,):
+            while True:
+                if self.childPipe.poll(None):
+                    data = self.childPipe.recv()
+                    self.motorPWM = data["motorPWM"]
+                    self.brakePWM = data["brakePWM"]
+                    self.steeringDegrees = data["steeringDegrees"]
+                    self.state = data["state"]
+                    # self.writeData(f'<{self.motorPWM}, {self.brakePWM}, {self.steeringDegrees}, {self.state}>')
+                    timeInSeconds = round(time.time() - 1682249887 - 31623000, 3)
+                    # print(timeInSeconds)
+                    # self.writeData(f'<{int(timeInSeconds)}, {int(timeInSeconds)- 300}, {timeInSeconds - 300}, moe>')
+                    self.writeData(f'<{self.motorPWM}, {self.brakePWM}, {self.steeringDegrees}, {self.state}>')
+
+                    time.sleep(0.005)
+
+                    # print(data)
+                    # match process:
+                    #     case "lineDetection":
+                    #         global lineDetectionData
+                    #         lineDetectionData = data
+                    #     case "yolov8":
+                    #         global objectDetectionData
+                    #         objectDetectionData = data
+
+    def connect(self, childPipe):
+        self.childPipe = childPipe
         print('Searching for connected ESP32...')
         self.ser = None
         while self.ser is None:
@@ -25,10 +58,20 @@ class ESP32:
 
         self.ser.flushInput()
         self.ser.flushOutput()
-        print('Connected to ' + self.ser.name)
+        print('Connected to ' + self.ser.name) 
+
+        parentDataThread = threading.Thread(target=self.getParentData)
+        parentDataThread.start()
+        while True:
+            self.receiveData()
 
     def writeData(self, data):
+        # ser.write("<Kaas, 1, 1.23456>".encode())
         self.ser.write(data.encode())
 
-    def receiveData(self, ):
-        pass
+    def receiveData(self,):
+            cc=str(self.ser.readline())
+            line = cc[2:][:-5]
+            if line != "":
+                print(line)
+            # print(self.ser.readline())
